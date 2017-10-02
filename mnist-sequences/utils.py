4e1de2bc2d82e5cb5ae36dbce3e9b5f33ee72252
Joseph Palermo <josephwpalermo@gmail.com>
2017-10-02 14:32:52 -0400

    some cleanup

--- mnist-sequences/utils.py
+++ mnist-sequences/utils.py
@@ -1,3 +1,4 @@
+import torch
 import bcolz
 
 def save_array(data, fname):
@@ -6,3 +7,14 @@ def save_array(data, fname):
 
 def load_array(fname):
     return bcolz.open(fname)[:]
+
+def one_hot_encode(value, value_range=11):
+    one_hot = torch.zeros(value_range)
+    one_hot[value] = 1
+    return one_hot
+
+def time_since(since):
+    s = time() - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
